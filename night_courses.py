25e5f205201e01d2426f653c5f0a180752916d23
James Kua <kua.james30@gmail.com>
2024-03-05 19:09:57 +0800

    Add faculty and department

--- night_courses.py
+++ night_courses.py
@@ -45,7 +45,7 @@ async def process_modules(modules, academic_year):
                     all_tutorial = [lesson for lesson in semester['timetable'] if lesson['lessonType'] in tutorialLessonTypes]
                     filtered_tutorial = [lesson for lesson in semester['timetable'] if int(lesson['startTime']) >= 1800 and lesson['lessonType'] in tutorialLessonTypes]
                     if (not all_lectures or any(filtered_lectures)) and (not all_tutorial or any(filtered_tutorial)) and (any(all_lectures) or any(all_tutorial)):
-                        night_courses_by_semester[semester['semester']].append([result['moduleCode'], result['moduleCredit'], result['title']])
+                        night_courses_by_semester[semester['semester']].append([result['moduleCode'], result['moduleCredit'], result['title'], result['faculty'], result['department']])
     return night_courses_by_semester
 
 def create_worksheet(workbook, semester, courses):
@@ -53,12 +53,16 @@ def create_worksheet(workbook, semester, courses):
     worksheet['A1'] = 'Code'
     worksheet['B1'] = 'Units'
     worksheet['C1'] = 'Name'
+    worksheet['D1'] = 'Faculty'
+    worksheet['E1'] = 'Department'
 
     for idx, course_info in enumerate(courses, start=2):
-        module_code, credit_units, course_name = course_info
+        module_code, credit_units, course_name, faculty, department = course_info
         worksheet.cell(row=idx, column=1, value=module_code.strip())
         worksheet.cell(row=idx, column=2, value=float(credit_units.strip()))
         worksheet.cell(row=idx, column=3, value=course_name.strip())
+        worksheet.cell(row=idx, column=4, value=faculty.strip())
+        worksheet.cell(row=idx, column=5, value=department.strip())
 
 def generate_html(courses_by_semester):
     html_content = """
@@ -77,10 +81,10 @@ def generate_html(courses_by_semester):
     for semester, courses in courses_by_semester.items():
         html_content += f"<h2>Semester {semester}</h2>"
         html_content += f"<div class=\"table-container\">"
-        html_content += "<table border='1'><tr><th>Code</th><th>Units</th><th>Name</th></tr>"
+        html_content += "<table border='1'><tr><th>Code</th><th>Units</th><th>Name</th><th>Faculty</th><th>Department</th></tr>"
         for course_info in courses:
-            module_code, credit_units, course_name = course_info
-            html_content += f"<tr><td>{module_code}</td><td>{credit_units}</td><td>{course_name}</td></tr>"
+            module_code, credit_units, course_name, faculty, department= course_info
+            html_content += f"<tr><td><a href='https://nusmods.com/courses/{module_code}' target='_blank'>{module_code}</a></td><td>{credit_units}</td><td>{course_name}</td><td>{faculty}</td><td>{department}</td></tr>"
         html_content += "</table>"
         html_content += "</div>"
     
